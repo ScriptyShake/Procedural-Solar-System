6a2cea0197e5482d788deb069d3e96534b2c5b3a
Valentina M. <70953225+ScriptyShake@users.noreply.github.com>
2025-03-16 22:33:40 +0100

    Added CF finder function, calculated lambda for poisson, but poisson isn't working yet. Also the ratio of Multiple/Single is wrong.

--- Python/Star Systems/stellar_multiplicity.py
+++ Python/Star Systems/stellar_multiplicity.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 #--------------------------------------------------------
 #           GENERATE M⊙primary FOLLOWING AN IMF
@@ -35,6 +36,7 @@ primary_masses = mass_list_lognormal + mass_list_powerlaw  # full list of primar
 
 # -------------- Multiplicity Properties table by Gaspard Duchêne and Adam Kraus (simplified)
 
+# Multiplicity Frequency (MF)
 def find_mf(m):
     if m < 0.1:
         return 0.22
@@ -49,21 +51,52 @@ def find_mf(m):
     elif m > 16:
         return 0.80
 
+# Companion Frequency (CF)
+def find_cf(m):
+    if m < 0.1:
+        return 0.22
+    elif 0.1 <= m <= 0.5:
+        return 0.33
+    elif 0.5 < m <= 1.3:
+        return 0.62
+    elif 1.3 < m <= 5:
+        return 1.00
+    elif 5 < m <= 16:
+        return 1.10
+    elif m > 16:
+        return 1.30
+
 # -------------- Find if star-system is single or multiple based on M⊙primary
+
+single_systems = []
+multiple_systems = []
+
+def calculate_lambda(cf):
+    return -np.log(1-cf)
+
+num_multiples = 0
+num_singles = 0
+
 for i in range(len(primary_masses)):
     mass = primary_masses[i]
     mf = find_mf(mass)
     random_number = np.random.uniform(0, 1)
     if random_number <= mf:
-        ...
-        # do logic for single
+        # logic for multiple systems
+        num_multiples += 1
+        cf = find_cf(mass)
+        #companion_num = scipy.stats.truncpoisson.rvs(1, calculate_lambda(cf))
+        #print(companion_num)
     else:
-        ...
-        # do logic for multiple
+        # logic for single systems
+        num_singles += 1
+        single_systems.append(mass)
 
 
-
-
+# PROBLEM: The ratio isn't correct, we're supposed to have approximately 1/3 of stars be multiple
+print("Number of Single-Star Systems: ", num_singles)
+print("Number of Multiple-Star Systems: ", num_multiples)
+print("Ratio of Multiple-Star Systems to Single-Star Systems: ", num_multiples/num_singles)
 
 
 
